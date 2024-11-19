762122c4febfedfe160b21a6eaa3b49d10277d1b
Reagan Rubio <123510533+reygunr@users.noreply.github.com>
2024-11-18 19:30:50 -0700

    Add files via upload
    Did problems 2 and 3.
    Changed the show options code to be in a loop.

--- Project3.py
+++ Project3.py
@@ -1,9 +1,13 @@
 import json
 import os
+import sys
 import mysql.connector
 from mysql.connector import Error
 
+db_connection = None
+
 def login_prompt():
+    global db_connection
     db_host = input("Enter the database host: ")
     db_name = input("Enter the database name: ")
     db_user = input("Enter the username: ")
@@ -18,9 +22,13 @@ def login_prompt():
         )
 
         if connection.is_connected():
+            db_connection = connection
+            print()
             print("Login successful!")
-            list_options(connection)
-            connection.close()
+            print()
+            
+            list_options()
+            db_connection.close()
         else:
             print("Connection failed. Please check your credentials.")
 
@@ -28,49 +36,50 @@ def login_prompt():
         print(f"Error: {e}")
 
 
-def list_options(db_connection):
-    print("1. Display all the digital displays")
-    print("2. Search digital displays given a scheduler system")
-    print("3. Insert a new digital display")
-    print("4. Delete a digital display")
-    print("5. logout")
-
-    user_input = input("Enter choice #: ")
-
-    if user_input.isdigit():
-        user_input = int(user_input)
-
-        if user_input <= 0:
-            print("Invalid choice. Try again")
-            list_options(db_connection)
-
-        if user_input > 5:
-            print("Invalid choice. Try again")
-            list_options(db_connection)
-
-        option_selection(db_connection, user_input)
-    else:
-        print("Invalid input. Please try again.\n")
-        list_options(db_connection)
-
-
-def option_selection(db_connection, selected_option):
-    if selected_option == 1:
-        display_all(db_connection)
-    elif selected_option == 2:
+def list_options():
+    
+    while True:
+        print("---------------------------------------------------------")
+        print("1. Display all the digital displays")
         print("2. Search digital displays given a scheduler system")
+        print("3. Insert a new digital display")
+        print("4. Delete a digital display")
+        print("5. logout")
+        print("---------------------------------------------------------")
+
+        user_input = input("Enter choice #: ")
+
+        if user_input.isdigit():
+            user_input = int(user_input)
+
+            if 0 >= user_input > 5:
+                print("Invalid choice. Try again")
+                continue
+
+
+            option_selection(user_input)
+        else:
+            print("Invalid input. Please try again.\n")
+            continue
+
+
+def option_selection(selected_option):
+    if selected_option == 1:
+        display_all()
+    elif selected_option == 2:
+        scheduler_search()
     elif selected_option == 3:
         print("3. Insert a new digital display")
+        insert_display()
     elif selected_option == 4:
         print("4. Delete a digital display")
     elif selected_option == 5:
-        logout(db_connection)
+        logout()
     else:
         print("Invalid input. Please try again.\n")
-        list_options(db_connection)
 
 
-def display_all(db_connection):
+def display_all():
     print("Displaying all digital displays")
     dbCursor = db_connection.cursor()
 
@@ -83,11 +92,75 @@ def display_all(db_connection):
 
     print("\n --done-- \n")
 
-    list_options(db_connection)
+    list_options()
+    
+def scheduler_search():
+    while True:
+        print()
+        print("Please select a scheduler system (Random, Smart, Virtue). Type Exit to exit")
+        scheduler_type = input().capitalize()
+        
+        if (scheduler_type == "Random" or
+            scheduler_type == "Smart" or
+            scheduler_type == "Virtue"):
+            with db_connection.cursor() as dbCursor:
+                statement = "SELECT * FROM DigitalDisplay WHERE schedulerSystem = %s;"
+                dbCursor.execute(statement, (scheduler_type,))
+                result = dbCursor.fetchall()
+                for row in result:
+                    print("\t", row)
+            break
 
-def logout(db_connection):
+        elif scheduler_type == "Exit" or scheduler_type == "exit":
+            break
+        else:
+            print("Invalid input. Valid inputs are: Random, Smart, or Virtue")
+    
+def insert_display():
+
+    with db_connection.cursor() as dbCursor:
+        print()
+        serialNo = input("Input the new display's serial number: ")
+        
+        if serialNo.isdigit():
+            serialNo = int(serialNo)
+            dbCursor.execute("SELECT serialNo FROM DigitalDisplay;")
+            result = dbCursor.fetchall()
+            for x in result:
+                if int(x[0]) == serialNo:
+                    print("\nInvalid Input. Serial number already in use.\n")
+                    return
+                    
+        else:
+            print("\nInvalid input. Please type in an integer.\n")
+            return
+        
+        scheduler = input("Input the new display's scheduler system (Random, Smart, Virtue): ").capitalize()
+        if not (scheduler == "Random" or scheduler == "Smart" or scheduler == "Virtue"):
+            print("Invalid input: please input either Random, Smart, or Virtue")
+            return
+        
+        modelNo = input("Input the new display's modelNo: ")
+        dbCursor.execute("SELECT modelNo FROM Model;")
+        result = dbCursor.fetchall()
+        for x in result:
+            if x[0] == modelNo:
+                break
+        else:
+            print("\nInvalid Input. Model number not in list of valid models.\n")
+            return
+        
+        statement = "insert into DigitalDisplay(serialNo, schedulerSystem, modelNo) values (%s, %s, %s)"
+        dbCursor.execute(statement, (serialNo, scheduler, modelNo))
+        print("\ndone!\n")
+        
+        
+    
+
+def logout():
     print("Logging out")
     db_connection.close()
+    sys.exit()
 
 if __name__ == "__main__":
     login_prompt()
